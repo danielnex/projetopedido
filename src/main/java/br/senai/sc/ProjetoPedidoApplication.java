2ca1887ca9e1f2077bd712f13a389fa907dae13d
Viviany Pfleger <pflegerviviany@gmail.com>
2019-02-26 21:24:26 -0300

    Projeto criado

---
+++ src/main/java/br/senai/sc/ProjetoPedidoApplication.java
@@ -0,0 +1,13 @@
+package br.senai.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPedidoApplication.class, args);
+	}
+
+}
