c986ba2f897538d0ca95c50e5d6e3c5c7a597645
Viviany Pfleger <pflegerviviany@gmail.com>
2019-02-26 21:54:28 -0300

    Criação classe Categoria

---
+++ src/main/java/br/senai/sc/domain/Categoria.java
@@ -0,0 +1,33 @@
+package br.senai.sc.domain;
+
+public class Categoria {
+
+	private Integer id;
+	private String nome;
+	
+	public Categoria(){
+	}
+
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
